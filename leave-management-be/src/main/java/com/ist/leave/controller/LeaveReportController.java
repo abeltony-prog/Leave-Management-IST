ed2878cdd896e580d6122d857e04a44890e90b83
Abeltony-prog <abeltony03@gmail.com>
2025-04-28 06:07:04 +0200

    temporary changees

--- leave-management-be/src/main/java/com/ist/leave/controller/LeaveReportController.java
+++ leave-management-be/src/main/java/com/ist/leave/controller/LeaveReportController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/api/v1/reports")
 @RequiredArgsConstructor
 public class LeaveReportController {
 
